0ce95fc41933c726d284aab382bccb0d18959a0f
Ozzy-renna <okerekeozzy123@gmail.com>
2025-08-22 13:22:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-/*const greeting=document.getElementById("greeting");
-const hour=new Date().getHours();
-  greeting.textContent = hour < 12
-      ? "Good Morning! Fresh pastries are ready."//quick if else statement
-      : "Good Afternoon! Treat yourself to something sweet.";*/
-      // Get the current hour
 const now = new Date();
 const hour = now.getHours();
 let greeting;
 
-// Decide greeting based on time
 if (hour < 12) {
   greeting = "Good morning! Welcome to The Peachy Crust Bakery.";
 } else if (hour < 18) {
@@ -16,7 +9,5 @@ if (hour < 12) {
 } else {
   greeting = "Good evening! End your day sweetly at The Peachy Crust Bakery.";
 }
-
-// Display greeting in an element with id="greeting"
 document.getElementById("greeting").innerText = greeting;
 
